5a7160686ca3081f675813962cbdf888534cfc1b
zack <zacharyzhangq1990@gmail.com>
2019-06-26 09:03:30 +0800

    header 组件 添加样式和js

--- src/common/header/index.jsx
+++ src/common/header/index.jsx
@@ -1,23 +1,76 @@
 import React, {Component} from 'react';
+import {CSSTransition} from 'react-transition-group';
 import {
   HeaderWrapper,
   Logo,
   Nav,
-  NavItem
+  NavItem,
+  SearchWrapper,
+  NavSearch,
+  // SearchInfo,
+  // SearchInfoTitle,
+  // SearchInfoSwitch,
+  // SearchInfoList,
+  // SearchInfoItem,
+  Addition,
+  Button,
 } from './style';
 
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      focused: false,
+    };
+    this.handleInputFocus = this.handleInputFocus.bind(this);
+    this.handleInputBlur = this.handleInputBlur.bind(this);
+  }
+handleInputFocus() {
+    this.setState({
+      focused: true
+    })
+
+}
+handleInputBlur() {
+    this.setState({
+      focused: false
+    })
+}
   render() {
     return (
       <HeaderWrapper>
         <Logo/>
         <Nav>
-          <NavItem className= 'left'> 首页</NavItem>
-          <NavItem className= 'left'>下载App</NavItem>
-          <NavItem className= 'right'>登录</NavItem>
-          <NavItem className= 'right'>Aa</NavItem>
+          <NavItem className='left'> 首页</NavItem>
+          <NavItem className='left'>下载App</NavItem>
+          <NavItem className='right'>登录</NavItem>
+          <NavItem className='right'>
+            <i className='iconfont'>&#xe636;</i>
+          </NavItem>
+          <SearchWrapper>
+            <CSSTransition
+              in={this.state.focused}
+              timeout={200}
+              className='slide'
+            >
+              <NavSearch
+             className = {this.state.focused ? 'focused' : null}
+             onFocus = { this.handleInputFocus }
+             onBlur = { this.handleInputBlur }
+              />
+              <i className={this.state.focused ? 'focused iconfont' :'iconfont'}>&#xe614;</i>
+            </CSSTransition>
+
+          </SearchWrapper>
         </Nav>
+        <Addition>
+          <Button className='writting'>
+            <i className='iconfont'>
+              &#xe615;
+            </i>
+          </Button>
+        </Addition>
       </HeaderWrapper>
     );
   }
--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style';
 import App from './App';
+import GlobalStyle from './style';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<React.Fragment><GlobalStyle /><App/></React.Fragment>, document.getElementById('root'));
