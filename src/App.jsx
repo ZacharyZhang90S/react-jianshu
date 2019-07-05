e623ce6d4c569f612dbb7a8c6dccfa116f898878
zack <zacharyzhangq1990@gmail.com>
2019-07-05 20:26:53 +0800

    添加路由功能

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import React, {Component} from 'react';
-import { Provider } from 'react-redux'
+import {Provider} from 'react-redux';
+import {BrowserRouter, Route} from 'react-router-dom';
 import Header from './common/header';
 import GlobalStyle from './style';
-import store from './store'
+import store from './store';
+import Home from './pages/home';
+import Detail from './pages/detail';
 
 class App extends Component {
   render() {
     return (
-      <Provider store = {store} ><GlobalStyle /><Header/></Provider>
+      <Provider store={store}>
+        <GlobalStyle/>
+        <Header/>
+        <BrowserRouter>
+          <Route path='/' exact component={Home}/>
+          <Route path='/detail' exact component={Detail}/>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
