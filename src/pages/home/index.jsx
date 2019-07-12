dd012c70aa0c77bafb1e6877008dba29fe1efe10
zack <zacharyzhangq1990@gmail.com>
2019-07-12 11:11:32 +0800

    pages 的 topic list 组件的完善

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,29 @@
 import React, {Component} from 'react';
+import Topic from './component/Topic';
+import List from './component/List';
+import Recommend from './component/Recommend';
+import Writer from './component/Writer';
+import {
+  HomeLeft,
+  HomeRight,
+  HomeWrapper,
+} from './style';
 
 class Home extends Component {
   render() {
     return (
-      <div> Home </div>
+      <HomeWrapper>
+        <HomeLeft>
+          <img className='banner-img' alt='banner-img'
+               src="https://upload.jianshu.io/admin_banners/web_images/4679/5570c9de5caff87a2ae4509d2b61d936fa974816.png?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540"/>
+          <Topic/>
+          <List/>
+        </HomeLeft>
+        <HomeRight>
+          <Recommend/>
+          <Writer/>
+        </HomeRight>
+      </HomeWrapper>
     );
   }
 }
