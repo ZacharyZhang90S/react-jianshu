8bbb128df06aa9d1c094c16b8a2ba01d807fd170
zack <zacharyzhangq1990@gmail.com>
2019-07-12 12:58:34 +0800

    优化代码

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,15 +1,15 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import Topic from './component/Topic';
-import List from './component/List';
-import Recommend from './component/Recommend';
-import Writer from './component/Writer';
-import axios from 'axios';
+import Topic from './components/Topic';
+import List from './components/List';
+import Recommend from './components/Recommend';
+import Writer from './components/Writer';
+import { actionCreators } from './store';
 
 import {
-  HomeLeft,
-  HomeRight,
   HomeWrapper,
+  HomeLeft,
+  HomeRight
 } from './style';
 
 class Home extends Component {
@@ -17,38 +17,28 @@ class Home extends Component {
     return (
       <HomeWrapper>
         <HomeLeft>
-          <img className='banner-img' alt='banner-img'
-               src="https://upload.jianshu.io/admin_banners/web_images/4679/5570c9de5caff87a2ae4509d2b61d936fa974816.png?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540"/>
-          <Topic/>
-          <List/>
+          <img className='banner-img' alt='' src="//upload.jianshu.io/admin_banners/web_images/4318/60781ff21df1d1b03f5f8459e4a1983c009175a5.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540" />
+          <Topic />
+          <List />
         </HomeLeft>
         <HomeRight>
-          <Recommend/>
-          <Writer/>
+          <Recommend />
+          <Writer />
         </HomeRight>
       </HomeWrapper>
-    );
+    )
   }
 
   componentDidMount() {
-    axios.get('/api/home.json').then((res) => {
-      const result = res.data.data;
-      const action = {
-        type: 'change_home_data',
-        topicList: result.topicList,
-        articleList: result.articleList,
-        recommendList: result.recommendList,
-      };
-      this.props.changeHomeData(action);
-    });
+    this.props.changeHomeData();
   }
 }
 
 const mapDispatch = (dispatch) => ({
-  changeHomeData(action) {
+  changeHomeData() {
+    const action = actionCreators.getHomeInfo();
     dispatch(action);
-
-  },
+  }
 });
 
 export default connect(null, mapDispatch)(Home);
--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -1,4 +1,25 @@
+// import { fromJS } from 'immutable';
+//
+// const defaultState = fromJS({
+//   topicList: [],
+//   articleList: [],
+//   recommendList: []
+// });
+//
+// export default (state = defaultState, action) => {
+//   switch(action.type) {
+//     case 'change_home_data':
+//       return state.merge({
+//         topicList: fromJS(action.topicList),
+//         articleList: fromJS(action.articleList),
+//         recommendList: fromJS(action.recommendList)
+//       })
+//     default:
+//       return state;
+//   }
+// }
 import { fromJS } from 'immutable';
+import * as constants from './constants';
 
 const defaultState = fromJS({
   topicList: [],
@@ -8,7 +29,7 @@ const defaultState = fromJS({
 
 export default (state = defaultState, action) => {
   switch(action.type) {
-    case 'change_home_data':
+    case constants.CHANGE_HOME_DATA:
       return state.merge({
         topicList: fromJS(action.topicList),
         articleList: fromJS(action.articleList),
