e17ede99239934922a5a7ccfae5f6718da4db1bf
zack <zacharyzhangq1990@gmail.com>
2019-07-17 14:18:38 +0800

    返回顶部功能实现

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,44 +1,77 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import Topic from './components/Topic';
 import List from './components/List';
 import Recommend from './components/Recommend';
 import Writer from './components/Writer';
-import { actionCreators } from './store';
+import {actionCreators} from './store';
+import {BackTop} from './style';
 
 import {
   HomeWrapper,
   HomeLeft,
-  HomeRight
+  HomeRight,
 } from './style';
 
 class Home extends Component {
+  handleScrollerTop() {
+    window.scrollTo(0, 0);
+  }
+
   render() {
     return (
       <HomeWrapper>
         <HomeLeft>
-          <img className='banner-img' alt='' src="//upload.jianshu.io/admin_banners/web_images/4318/60781ff21df1d1b03f5f8459e4a1983c009175a5.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540" />
-          <Topic />
-          <List />
+          <img className='banner-img' alt=''
+               src="//upload.jianshu.io/admin_banners/web_images/4318/60781ff21df1d1b03f5f8459e4a1983c009175a5.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540"/>
+          <Topic/>
+          <List/>
         </HomeLeft>
         <HomeRight>
-          <Recommend />
-          <Writer />
+          <Recommend/>
+          <Writer/>
         </HomeRight>
+        {this.props.showScroll ? <BackTop onClick={this.handleScrollerTop}>顶部</BackTop> : null}
       </HomeWrapper>
-    )
+    );
   }
 
   componentDidMount() {
     this.props.changeHomeData();
+    this.bindEvents();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.props.changeScrollTopShow);
+  }
+
+  bindEvents() {
+    window.addEventListener('scroll', this.props.changeScrollTopShow);
   }
 }
 
 const mapDispatch = (dispatch) => ({
   changeHomeData() {
-    const action = actionCreators.getHomeInfo();
-    dispatch(action);
-  }
+    dispatch(actionCreators.getHomeInfo());
+  },
+  changeScrollTopShow() {
+    if (document.documentElement.scrollTop > 100) {
+      console.log(1);
+      console.log(document.documentElement.scrollTop);
+
+      dispatch(actionCreators.toggleTopShow(true));
+    } else {
+      console.log(2);
+      console.log(document.documentElement.scrollTop);
+
+      dispatch(actionCreators.toggleTopShow(false));
+    }
+  },
 });
 
-export default connect(null, mapDispatch)(Home);
+const mapState = (state) => ({
+  showScroll: state.getIn(['home', 'showScroll']),
+
+});
+
+export default connect(mapState, mapDispatch)(Home);
