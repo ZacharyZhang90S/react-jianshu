fa7e694374727d3ff52b601dd964c1ef346a6146
zack <zacharyzhangq1990@gmail.com>
2019-07-25 10:51:45 +0800

    打包项目上传

---
+++ build/precache-manifest.711753d5423eabf99fee41b56afdc649.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2352e62f86cdd87921b6fdbc0c881714",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93837af0a6da900733a8",
+    "url": "/static/js/2.efd7dc05.chunk.js"
+  },
+  {
+    "revision": "8dafe09a7a6562347c6a",
+    "url": "/static/js/3.0d8325f8.chunk.js"
+  },
+  {
+    "revision": "4cf43e881cc3acf4fe11",
+    "url": "/static/js/main.5be9a6a6.chunk.js"
+  },
+  {
+    "revision": "4866fe7540f3af6e0e61",
+    "url": "/static/js/runtime~main.504302e7.js"
+  }
+]);
